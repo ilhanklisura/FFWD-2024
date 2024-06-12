5900e5d29d4bacfce5fbeba8ec6c2ea8185e28a6
ilhankDev <ilhank.official@gmail.com>
2024-06-12 11:25:52 +0200

    Navbar (Responsive bug fix)

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -1,29 +1,91 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+   AppBar,
+   Toolbar,
+   Typography,
+   Button,
+   IconButton,
+   Drawer,
+   List,
+   ListItem,
+   ListItemText,
+   Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+   const [drawerOpen, setDrawerOpen] = useState(false);
+
+   const toggleDrawer = (open) => (event) => {
+      if (
+         event.type === "keydown" &&
+         (event.key === "Tab" || event.key === "Shift")
+      ) {
+         return;
+      }
+      setDrawerOpen(open);
+   };
+
+   const menuItems = [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about" },
+      { text: "Portfolio", link: "/portfolio" },
+      { text: "Posts", link: "/posts" },
+      { text: "Contact", link: "/contact" },
+   ];
+
+   const list = () => (
+      <Box
+         sx={{ width: 250 }}
+         role="presentation"
+         onClick={toggleDrawer(false)}
+         onKeyDown={toggleDrawer(false)}
+      >
+         <List>
+            {menuItems.map((item, index) => (
+               <ListItem button key={index} component={Link} to={item.link}>
+                  <ListItemText primary={item.text} />
+               </ListItem>
+            ))}
+         </List>
+      </Box>
+   );
+
    return (
       <AppBar position="static">
          <Toolbar>
-            <Typography variant="h6" style={{ flexGrow: 1 }}>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
                Ilhan Klisura | Software Developer
             </Typography>
-            <Button color="inherit" component={Link} to="/">
-               Home
-            </Button>
-            <Button color="inherit" component={Link} to="/about">
-               About
-            </Button>
-            <Button color="inherit" component={Link} to="/portfolio">
-               Portfolio
-            </Button>
-            <Button color="inherit" component={Link} to="/posts">
-               Posts
-            </Button>
-            <Button color="inherit" component={Link} to="/contact">
-               Contact
-            </Button>
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+               {menuItems.map((item, index) => (
+                  <Button
+                     color="inherit"
+                     component={Link}
+                     to={item.link}
+                     key={index}
+                  >
+                     {item.text}
+                  </Button>
+               ))}
+            </Box>
+            <Box sx={{ display: { xs: "block", md: "none" } }}>
+               <IconButton
+                  color="inherit"
+                  edge="start"
+                  onClick={toggleDrawer(true)}
+               >
+                  <MenuIcon />
+               </IconButton>
+            </Box>
+            <Drawer
+               anchor="right"
+               open={drawerOpen}
+               onClose={toggleDrawer(false)}
+            >
+               {list()}
+            </Drawer>
          </Toolbar>
       </AppBar>
    );
