e2d197366251396cab0186baf8d03fbfe94531c3
ilhankDev <ilhank.official@gmail.com>
2024-06-13 18:54:01 +0200

    Removed labs and bugfixes.

--- labs/lab6/lab-exercise1/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-   $("#button_change").click(function () {
-      var colorToChangeTo = $("#input_color").val();
-      $(".cow_color").text(colorToChangeTo);
-   });
-});
